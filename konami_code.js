ed309ef6e38631aede293cafb87635d9a720c4f5
Oomkar Pandit <oomkar@yahoo.com>
2018-08-19 15:51:48 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,30 @@ const codes = [
   "b",
   "a"
 ];
+ let index=0
+ function init(){
+   document.body.addEventListener('keydown',function(e){console.log('Listening'+e.key)
+     keydown(e)
+   })
+ }
+ function keydown(e){
+   console.log('Listening inside func'+e.key)
+  const key=e.key
+  if (key===codes[index]){
+    index++
+    console.log(index+' this is number correct these many times'+'total length = '+codes.length)
+    if (index===codes.length){
+      window.alert("hurray");
+      console.log('mission complete')
+      index=0
+      console.log('index now '+index)
+    
+    
+      
+    }
+  }
+  else {
+    index=0
 
-function init() {
-  // your code here
-}
+ }
+ }
